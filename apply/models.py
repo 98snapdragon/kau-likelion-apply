1d2acf2797fbb141e18c3d6a2adf069d63a4b283
98snapdragon <98snapdragon@gmail.com>
2022-02-21 20:53:20 +0900

    model update

--- apply/models.py
+++ apply/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Apply(models.Model):
-    name = models.CharField(max_length=20)
-    studentId = models.CharField(max_length=20)
-    grade = models.CharField(max_length=10)
-    major = models.CharField(max_length=30)
-    email = models.CharField(max_length=40)
-    phone = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    studentId = models.CharField(max_length=100)
+    grade = models.CharField(max_length=100)
+    major = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
 
-    motive = models.TextField(max_length=499)
+    motive = models.TextField()
     position = models.CharField(max_length=10)
-    positionRe = models.TextField(max_length=499)
-    teamwork = models.TextField(max_length=499)
-    passion = models.TextField(max_length=499)
-    code = models.TextField(max_length=499)
+    positionRe = models.TextField()
+    teamwork = models.TextField()
+    passion = models.TextField()
+    code = models.TextField()
